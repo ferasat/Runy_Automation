45330a6a7c8b71738a28c9f9e3b57f5da8cb95c1
امین فراست <amin.ferasat@gmail.com>
2022-06-13 21:13:35 +0430

    Test 2git

---
+++ resources/views/livewire/users/rp/index-rp.blade.php
@@ -0,0 +1,176 @@
+<div class="col">
+    <div class="row">
+        <div class="col">
+            <h2 class="text-white  aaa">Departments</h2>
+        </div>
+    </div>
+
+    <div class="row mb-5">
+        <div class="col-xl-3 col-lg-6">
+            <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Mailroom</h5>
+                            <span class="h2 font-weight-bold mb-0">350,897</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                <i class="fas fa-chart-bar"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                        <span class="text-nowrap">Since last month</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+            <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">For Test</h5>
+                            <span class="h2 font-weight-bold mb-0">2,356</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+                                <i class="fas fa-chart-pie"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
+                        <span class="text-nowrap">Since last week</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+            <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Users</h5>
+                            <span class="h2 font-weight-bold mb-0">24</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                                <i class="fas fa-users"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
+                        <span class="text-nowrap">Since yesterday</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-6">
+            <div class="card card-stats mb-4 mb-xl-0">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Requests</h5>
+                            <span class="h2 font-weight-bold mb-0">1500</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-info text-white rounded-circle shadow">
+                                <i class="fas fa-percent"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                        <span class="text-nowrap">Since last month</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="row align-items-center">
+                <div class="col-8">
+                    <h3 class="mb-0">Roles</h3>
+                    <p class="text-sm mb-0">
+                        This is an example of role management. This is a minimal setup in order to get started fast.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="table-responsive ">
+            <table class="table table-flush ">
+                <thead class="thead-light">
+                <tr role="row">
+                    <th> Name</th>
+                    <th>Description</th>
+                    <th>Creation date</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+
+
+                <tr role="row" class="odd">
+                    <td class="sorting_1">Admin</td>
+                    <td>This is the administration role</td>
+                    <td>12/06/2022 18:30</td>
+                    <td class="text-right">
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item"
+                                   href="https://argon-dashboard-pro-laravel.creative-tim.com/role/1/edit">Edit</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr role="row" class="even">
+                    <td class="sorting_1">Creator</td>
+                    <td>This is the creator role</td>
+                    <td>12/06/2022 18:30</td>
+                    <td class="text-right">
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item"
+                                   href="https://argon-dashboard-pro-laravel.creative-tim.com/role/2/edit">Edit</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr role="row" class="odd selected">
+                    <td class="sorting_1">Member</td>
+                    <td>This is the member role</td>
+                    <td>12/06/2022 18:30</td>
+                    <td class="text-right">
+                        <div class="dropdown">
+                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                <a class="dropdown-item"
+                                   href="https://argon-dashboard-pro-laravel.creative-tim.com/role/3/edit">Edit</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
